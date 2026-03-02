b95c1bb7845c4ed154a1f679e34ecec5acc667f1
Keddib <52551794+Keddib@users.noreply.github.com>
2026-03-02 14:14:48 +0100

    v3.2.2 — remove microphone requirement from passive liveness

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DiditSDKBinary",
-            url: "https://github.com/didit-protocol/sdk-ios/releases/download/3.2.1/DiditSDK.xcframework.zip",
-            checksum: "d843619e1025eaa0e4a3bd92972b84b42c9fcea5133b32b7e4b5bd2651c3ea4b"
+            url: "https://github.com/didit-protocol/sdk-ios/releases/download/3.2.2/DiditSDK.xcframework.zip",
+            checksum: "3a1babf73351a6693deba7f1ebeae48a72e09b381fbab8215d8173e9f04cb9f1"
         ),
         .binaryTarget(
             name: "OpenSSLBinary",
-            url: "https://github.com/didit-protocol/sdk-ios/releases/download/3.2.1/OpenSSL.xcframework.zip",
-            checksum: "d6a4043e2fc0885553c93f95b07f3fac2fc1a193b8f7c977b763287e5d125224"
+            url: "https://github.com/didit-protocol/sdk-ios/releases/download/3.2.2/OpenSSL.xcframework.zip",
+            checksum: "60fdb31be2e90c8d1d22720affe77b1be249c781a8a995981b0c60ab46db421a"
         )
     ]
 )
